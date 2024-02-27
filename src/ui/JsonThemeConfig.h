cc8c9490a2ee94c93cb7c3ddf535db566928b676
Kirill Orlov <me@kirillorlov.pro>
2024-02-28 00:44:26 +0100

    added json schema

---
+++ src/ui/JsonThemeConfig.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <optional>
+
+struct Variable {
+  std::string name;
+  std::string path;
+  std::optional<std::string> stringFormat;
+  std::string type;
+};
+
+struct Request {
+  std::string id;
+  std::string url;
+  std::vector<Variable> variables;
+};
+
+enum class Size : int { LARGE, SMALL };
+
+enum class Type : int { ARC, IMAGE, TEXT, TIMER };
+
+struct Content {
+  std::optional<Size> size;
+  Type type;
+  std::optional<std::string> value;
+  std::optional<double> x;
+  std::optional<double> y;
+  std::optional<std::string> file;
+  std::optional<bool> loop;
+  std::optional<double> duration;
+};
+
+struct Transition {
+  std::string condition;
+  std::string target;
+};
+
+struct Scene {
+  std::optional<std::string> background;
+  std::vector<Content> content;
+  std::string id;
+  std::vector<Transition> transitions;
+};
+
+struct Variables {
+  std::string background;
+};
+
+struct Coordinate {
+  std::string firmwareUpdateScene;
+  std::string noKlipperScene;
+  std::vector<Request> requests;
+  std::vector<Scene> scenes;
+  std::string startingScene;
+  Variables variables;
+};
