da544336eaf79a61b242a7d9c6f7753349758267
Kirill Orlov <me@kirillorlov.pro>
2024-05-26 21:53:03 +0200

    Fix button on Knomi v2

--- src/button/Button.h
+++ src/button/Button.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../config/Config.h"
+#include "../hal/pinout.h"
 #include "./network/WifiManager.h"
 #include <Arduino.h>
 #include <Ticker.h>
@@ -7,12 +8,48 @@
 class Button final {
 
 public:
-  explicit Button(WifiManager *mgr, Config *config);
-  void KeyScan();
+  explicit Button(WifiManager *mgr, Config *config) {
+    this->config = config;
+    this->manager = mgr;
+#if KNOMIV1
+    pinMode(BOOT_PIN, INPUT); // input mode; default -> high, pressed -> low.
+#else
+#if KNOMIV2
+    pinMode(BOOT_PIN, INPUT_PULLUP); // input mode; default -> high, pressed -> low.
+#endif
+#endif
+    delay(200);
+    currentState = HIGH;
+    lastState = HIGH;
+
+    timer.attach(0.001, Button::KeyScan, this);
+  }
+
+  void KeyScan() {
+    currentState = (uint8_t)digitalRead(0); // RESET_PIN
+
+    if (lastState == HIGH && currentState == LOW) { // button is pressed
+      LV_LOG_INFO("Button pressed");
+      pressedTime = millis();
+    } else if (lastState == LOW && currentState == HIGH) { // button is released
+      LV_LOG_INFO("Button released");
+      releasedTime = millis();
+
+      unsigned long pressDuration = releasedTime - pressedTime;
+
+      if (pressDuration > WIFI_RESET_LONG_PRESS_MS) {
+#if !DEBUG
+        ESP.restart();
+#endif
+        manager->resetWifi();
+      }
+    }
+    lastState = currentState;
+  }
 
   static void KeyScan(Button *button) { button->KeyScan(); }
 
-  bool isPressed() const;
+  bool isPressed() const { return lastState == LOW; }
 
 private:
   Config *config;
