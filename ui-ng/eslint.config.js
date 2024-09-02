a2a1e837e82bf65d642e1cce71ecae65f161680e
Jordan Maddrell <jordan.maddrell@gmail.com>
2024-09-02 20:46:38 +0100

    Start of eslint ts config

--- ui-ng/eslint.config.js
+++ ui-ng/eslint.config.js
@@ -1,13 +1,15 @@
 import eslintPluginSvelte from "eslint-plugin-svelte";
+import tseslint from 'typescript-eslint';
 export default [
     // add more generic rule sets here, such as:
     // js.configs.recommended,
     ...eslintPluginSvelte.configs["flat/recommended"],
+    ...tseslint.configs.recommended,
     {
         rules: {
             // override/add rules settings here, such as:
             // 'svelte/rule-name': 'error'
             semi: "error",
-        },
+        }
     },
 ];
