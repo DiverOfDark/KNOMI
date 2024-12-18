6041bc89236cc99978b508a777453595979fd938
Jordan Maddrell <jordan.maddrell@gmail.com>
2024-12-18 18:03:13 +0000

    Upgrade UI to svelete 5

--- ui-ng/src/main.ts
+++ ui-ng/src/main.ts
@@ -1,8 +1,12 @@
+import { mount } from 'svelte';
 import App from "./App.svelte";
 import "./App.scss";
 
-const app = new App({
-    target: document.getElementById("app"),
-});
+const target = document.getElementById("app");
 
+if (!target) {
+    throw new Error("Target not found");
+}
+
+const app = mount(App, { target });
 export default app;
